1d0cd7e872f860530cc32e524457958fd0f9aa20
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-15 13:46:11 -0300

    :construction: build: marcacoes html curricul card

--- src/CurriculoComponent.jsx
+++ src/CurriculoComponent.jsx
@@ -1,5 +1,12 @@
 export default function CurriculoComponent() {
     return (
-        <div className="black-card-curriculo"></div>
+        <div className=" bg-black black-card-curriculo">
+            <div className="border-gradient curriculo-border">
+                <div className="inside-gradient bkg-black">
+                    <img src="A4-1.png" alt="" className="curriculo-img"/>
+                </div>
+            </div>
+            <a className="link-curriculo" href="Curriculo-Ruan.pdf" download>VEJA MEU CURRICULO</a>
+        </div>
     )
 }
