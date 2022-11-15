194595941059656e550b85750a1a49970651e54f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-15 19:17:50 -0300

    :construction: build: adiciona Projetos a App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './AboutMe.css';
 import './Title.css';
 import MainCard from './MainCard';
 import AboutMe from './AboutMe';
+import Projetos from './Projetos';
 
 function App() {
   return (
     <div className='app'>
       <MainCard />
-      <AboutMe/>
+      <AboutMe />
+      <Projetos/>
     </div>
   );
 }
