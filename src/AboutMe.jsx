4b89a2cd396400c9594d276724b9f1d2584c030f
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-15 10:09:09 -0300

    :construction: build: cria section aboutme

---
+++ src/AboutMe.jsx
@@ -0,0 +1,12 @@
+import VsScreenComponent from "./VsScreenComponent";
+
+function AboutMe() {
+    return (
+        <div className="AboutMe">
+            <h2>SOBRE MIM</h2>
+            <VsScreenComponent/>
+        </div>
+    )
+}
+
+export default AboutMe;
