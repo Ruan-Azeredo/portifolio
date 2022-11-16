ac6b20bfe24a67b578a5eaafa026e88097cf71ba
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-15 23:30:13 -0300

    :sparkles: feat: card projetos

---
+++ src/ProjetoCard.jsx
@@ -0,0 +1,19 @@
+export default function ProjetoCard(props) {
+    return (
+        <div className="card-black">
+            <div className="nav-card">
+                <div className="card-name">{props.name}</div>
+                <div className="icons-card">
+                    <img src={props.icon1} alt="" />
+                    <img src={props.icon2} alt="" />
+                    <img src={props.icon3} alt="" />
+                </div>
+            </div>
+            <img className="img-card" src={props.screen} alt="" />
+            <div className="infos-card">
+                <p className="txt-card">{props.description}</p>
+                <a className="git-button"><img src="github-icon.png" alt=""/></a>
+            </div>
+        </div>
+    )
+}
