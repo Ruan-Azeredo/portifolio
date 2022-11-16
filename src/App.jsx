657eebb21e267bec143056334653ac38425794fc
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-16 12:01:36 -0300

    :construction: build: adiciona trajetoria a App

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import './App.css';
 import './AboutMe.css';
 import './Title.css';
 import './Projetos.css'
+import './Trajetoria.css'
 import MainCard from './MainCard';
 import AboutMe from './AboutMe';
 import Projetos from './Projetos';
+import Trajetoria from './Trajetoria';
 
 function App() {
   return (
     <div className='app'>
       <MainCard />
       <AboutMe />
-      <Projetos/>
+      <Projetos />
+      <Trajetoria/>
     </div>
   );
 }
