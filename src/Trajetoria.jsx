bf9284b8adf39c2555f46993988cd16d054230f0
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-16 12:00:32 -0300

    :construction: build: adiciona cards traj

---
+++ src/Trajetoria.jsx
@@ -0,0 +1,21 @@
+import CardComponent from "./CardComponent";
+import Title from "./Title";
+
+export default function Trajetoria() {
+    return (
+        <div>
+            <Title title='Trajetoria' grayscale={true}/>
+            <div className="time-line-container">
+                <CardComponent name='UFES' logo='logo-ufes.png' description='Entro na Empresa Junior da Comnputação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como progrmar e ter a conte....'/>
+                <div className="time-line">
+                    <div className="date-container">
+                        <div>ABR/2021</div><div className="time-circle c-1"></div>
+                        <div className="time-line-gradient"></div>
+                        <div className="time-circle c-2"></div><div>ABR/2021</div>
+                    </div>
+                </div>
+                <CardComponent name='Adapti' logo='logo-adapti.png' description='Entro na Empresa Junior da Comnputação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como progrmar e ter a conte....'/>
+            </div>
+        </div>
+    )
+}
