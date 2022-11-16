844db615e24def325dbf9311d90bd6be66b93e08
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-16 09:58:30 -0300

    :sparkles: feat: adiciona variacao de Titulo

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,5 +1,12 @@
-export default function Title(props){
-    return (
-        <h2 className="title">{ props.title }</h2>
-    )
+export default function Title(props) {
+    const grayscale = props.grayscale
+    if (grayscale) {
+        return (
+            <h2 className="title grayscale">{ props.title }</h2>
+        )   
+    } else {
+        return (
+            <h2 className="title bluescale">{ props.title }</h2>
+        )
+    }
 }
