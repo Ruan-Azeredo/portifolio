89d4120d863146bed8577c2def8a3426677cd3b6
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-16 22:52:02 -0300

    :construction: build: adiciona tag para links

--- src/CardContComponent.jsx
+++ src/CardContComponent.jsx
@@ -3,23 +3,27 @@ export default function CardContComponent(props) {
 
     if (!grayscale) {
         return (
-            <div className="minicard-contato">
-                <div className="icon-label">
-                    <img src={props.icon} alt="" />
-                    <div>{props.name}</div>
+            <a href={`${props.link}`}>
+                <div className="minicard-contato">
+                    <div className="icon-label">
+                        <img src={props.icon} alt="" />
+                        <div>{props.name}</div>
+                    </div>
+                    <div className="minicard-route">{props.route}</div>
                 </div>
-                <div className="minicard-route">{props.route}</div>
-            </div>
+            </a>
         )
     } else {
         return (
-            <div className="minicard-contato-gray">
-                <div className="icon-label">
-                    <img src={props.icon} alt="" />
-                    <div>{props.name}</div>
+            <a href={`${props.link}`}>
+                <div className="minicard-contato-gray">
+                    <div className="icon-label">
+                        <img src={props.icon} alt="" />
+                        <div>{props.name}</div>
+                    </div>
+                    <div className="minicard-route">{props.route}</div>
                 </div>
-                <div className="minicard-route">{props.route}</div>
-            </div>
+            </a>
         )
     }
 }
