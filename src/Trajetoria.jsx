22b89bb2b1fdf33193746ded77c1d46c6e7902a6
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-19 16:19:21 -0300

    :bug: fix: comnputação -> computação

--- src/Trajetoria.jsx
+++ src/Trajetoria.jsx
@@ -12,7 +12,7 @@ export default function Trajetoria() {
                     </div>
                     <div className="date-container date-r">
                         <div className="date">SET/2021</div><div className="time-circle c-2"></div>
-                        <CardComponent name='Adapti' logo='logo-adapti.png' description='Entrei na Empresa Junior da Comnputação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como programar e ter a contato com o mundo do desenvolvimento web.'/>
+                        <CardComponent name='Adapti' logo='logo-adapti.png' description='Entrei na Empresa Junior da Computação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como programar e ter a contato com o mundo do desenvolvimento web.'/>
                     </div>  
                     <div className="time-line-gradient"></div>
                 </div>
