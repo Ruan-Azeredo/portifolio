81ff9ae2808faf3e53ab2526787f3499bea744ae
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-11-25 21:46:22 -0300

    :construction: build: card black

---
+++ src/CardBlack.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const axios = require('axios')
+
+export default function CardBlack() {
+
+    return (
+        <div>
+            oiiiiiiiii
+        </div>
+    )
+}
