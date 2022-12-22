274f3e86a5d2905b3bba4af936ed853c338b09f7
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-22 11:18:42 -0300

    :sparkles: feat: cria component de card

--- src/CardBlack.jsx
+++ src/CardBlack.jsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
-
-const axios = require('axios')
-
-export default function CardBlack() {
+export default function CardBlack(props) {
 
     return (
-        <div>
-            oiiiiiiiii
+        <div className='out-gradient'>
+            <div className='in-gradient'>
+                <div className='card-bg-black'>
+                    {props.children}
+                </div>
+            </div>
+
+            <style jsx>{`
+                    .conteudo{
+                        width: ${props.width};
+                    }
+
+                    .card-bg-black{
+                        background-color: #202020;
+                        padding: 20px;
+                        width: fit-content;
+                        border-radius: 10px;
+                        position: relative;
+                        top: -10px;
+                        left: -10px;           
+                    }
+
+                    .out-gradient{
+                        background: linear-gradient(to right, var(--azulescuro), var(--azulclaro));
+                        padding: 2px;
+                        width: fit-content;
+                        border-radius: 10px;
+                        position: relative;
+                        top: 10px;
+                        left: 10px;
+                    }
+
+                    .in-gradient{
+                        background: #2C3333;
+                        width: fit-content;
+                        border-radius: 10px;
+                    }
+                `}</style>
         </div>
     )
 }
