21748a7f0c1cce0c6a26eccc8ad80ebf814137ea
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-22 14:23:47 -0300

    :recycle: refactor: cria page home para rota '/'

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import MainCard from './MainCard';
+import AboutMe from './AboutMe';
+import Projetos from './Projetos';
+import Trajetoria from './Trajetoria';
+import Contato from './Contato';
+import Footer from './Footer';
+
+export default function Home() {
+    return (
+        <div>
+            <MainCard />
+            <AboutMe />
+            <Projetos />
+            <Trajetoria />
+            <Contato />
+            <Footer />
+        </div>
+    )
+}
