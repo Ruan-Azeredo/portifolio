da3ec40f495785fb05ce9f5add6ecfec851b99cd
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-22 15:56:24 -0300

    :sparkles: feat: fucnctionalidade row/row-reverse

--- src/ConteudoPageProj.jsx
+++ src/ConteudoPageProj.jsx
@@ -5,12 +5,21 @@ export default function ConteudoPageProj() {
     const img = 'telamybooks.png'
     const txt = 'O Front deste projeto foi desenvolvido com o Next, que é um Framework que funciona utilizando a biblioteca React, e que permite uma renderização Server Side. Para estilização, foi utilizado o Framework Tailwind CSS. Ainda no projeto em Next foram adicionadas outras dependencias para gereciamento de cookies, autenticação com a firebase e comunicação com a API do Back-end.'
 
+    const flexDirection = 'row-reverse'
+
     return (
         <div className="conteudo-container">
             <CardBlack>
                 <img src={img} />
             </CardBlack>
             <div>{txt}</div>
+
+            <style jsx>{`
+                .conteudo-container{
+                    display: flex;
+                    flex-direction: ${flexDirection}
+                }
+            `}</style>
         </div>
     )
 }
