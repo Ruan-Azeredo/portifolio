7641c4d9cff0c9431131d041cbf4c26cbdaa1b1b
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-22 23:50:40 -0300

    :sparkles: feat: atualiza txt mybooks

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
               }
             ], back: [
               {
-                img: 'techmybooks.png',
-                txt: 'O Front deste projeto foi desenvolvido com o Next, que é um Framework que funciona utilizando a biblioteca React, e que permite uma renderização Server Side. Para estilização, foi utilizado o Framework Tailwind CSS. Ainda no projeto em Next foram adicionadas outras dependencias para gereciamento de cookies, autenticação com a firebase e comunicação com a API do Back-end.'
+                img: '',
+                txt: 'A fazer...'
               }
             ]
           }}
