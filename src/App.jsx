4abeaf29a11fc508fffc34d48965cccc46b58780
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-26 15:02:59 -0300

    :feat: sparkles: add infos array mybooks

--- src/App.jsx
+++ src/App.jsx
@@ -34,11 +34,26 @@ function App() {
               }, {
                 img: 'arquitemybooks.png',
                 txt: 'A estruturação do projeto foi feita utilizando o principio nativo de componentização do React. Dividi o projeto em pastas de Pages, que contem minhas páginas principais, Functions, que comporta as funções de requisição em axios e outras funções que são usadas em varios locais do sistema, Context que armazenam os Hooks globais, Conponents, que guarda os componentes JSX e o Content que possui componentes que comportão outros elementos.'
+              }, {
+                img: 'loginmybooks.png',
+                txt: 'O projeto possui a tela de login, com autenticação, através do google, que é desenvolvido através do uso do firebase, o login pode ser feito com o nome, email e senha ou pela login com o google. O login do usuario permanece no sistema através dos cookies e, os quais quando expiram, ou quando o usuário fizer o logout, cada rota do site será redirecionada para a tela de login novamente.'
+              }, {
+                img: 'modalmybooks.png',
+                txt: 'A logica do sistema consiste em no usuário cadastrar seus autores, dentro de autores os livros referentes, e dentro destes livros cadastrar as resenhas, que são o objetivo principal do usuário. O sistema possui a funcionalidade de além de cadastro de todos objetos descritos acima, também permite visualizar, editar e excluir todos os dados.'
+              }, {
+                img: 'darkmodemybooks.png',
+                txt: 'O sistema possui como features o modo escuro, que salva a preferencia do usuário no localhost do navegador, e também possui um card de informações do usuário.'
               }
             ], back: [
               {
-                img: '',
-                txt: 'A fazer...'
+                img: 'techbackmybooks.png',
+                txt: 'O Back-end do sistema foi feito utilizando Node.js, com o framework Express, com o Nodemon para auxiliar no desenvolvimento, com o Sequelize para a estrutura do projeto com o banco de dados, que é feito com PostgreSQL, utilizando o Docker, para gerar o container do banco de dados, também foi utilizado o Cors para a validação das rotas de API, e o Multer para o arquivamento e manipulação de imagens.'
+              }, {
+                img: 'arqbackmybooks.png',
+                txt: 'A API foi arquitetada da seguinte forma, com um config, que faz a comunicação com o o container que roda o PostgreSQL, as migrations e os models possuem o as estruturas das tabelas dos bancos de dados, que são as tabelas de usuários, autores, livros e resenhas respectivamente. A pasta public comporta as imagens que são arquivadas atravez do Multer.'
+              }, {
+                img: 'controllerDeleteBooks.png',
+                txt: 'Os Controllers são onde são definidas efetivamente as rotas de API, aqui temos um exemplo, da rota de delete dos BookController, que alem de apagar o livro, precisa apagar a imagem que está salva em /public.'
               }
             ]
           }}
