2939b4bef36ab662870b54ac0719b26dffe9b548
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-17 11:04:59 -0300

    :recycle: refactor: card trajetoria

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,14 +1,31 @@
 export default function CardComponent(props) {
+
+    var fl, end
+    if (props.name == 'Adapti') {
+        fl = 'flex-col-reverse'
+        end = 'items-end'
+    } else {
+        fl = 'flex-col'
+        end = 'items-start'
+    }
+
     return (
-        <div className="card-traj">
-            <h3 className="title-traj">{props.name}</h3>
-            <div className="card-infos-traj">
-                <div className="bg-logo-traj">
-                    <img src={props.logo} alt="" />
+        <div className={`flex ${fl} relative -top-[161px] z-30 px-10`}>
+            <div className="bg-black h-36 p-2 flex rounded-lg items-center">
+                <div className="bg-gradient-to-br from-white to-gray w-28 h-28 grid items-center p-2 rounded-md relative -left-7">
+                    <img src={props.logo} alt=""/>
                 </div>
-                <p className="description-traj">{props.description}</p>
+                <div className="text-sm text-white pr-2 max-w-sm">
+                    <div className="font-bold">{props.name}</div>
+                    <div>{props.description}</div>
+                </div>
             </div>
-            <div className="gradient-bot"></div>
+            <div className="bg-white rounded-full h-3 w-3 mx-10 my-3">
+
+            </div>
+            <div className={`h-36 grid ${end}`}>
+                <div className="bg-white px-3 py-1 rounded-lg w-fit">{props.time}</div>
+            </div>
         </div>
     )
 }
