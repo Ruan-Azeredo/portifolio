86a1eec23d1aa03f8c50d5526a245fee47c2b06b
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-17 11:05:27 -0300

    :fire: choer: remode tags desnecessarias

--- src/sections/Trajetoria.jsx
+++ src/sections/Trajetoria.jsx
@@ -6,14 +6,8 @@ export default function Trajetoria() {
         <div className="trajetoria-container"><a name='trajetoria'></a>
             <Title title='Trajetoria' grayscale={true}/>
                 <div className="time-line">
-                    <div className="date-container date-l">
-                        <div className="date">ABR/2021</div><div className="time-circle c-1"></div>
-                        <CardComponent name='UFES' logo='logo-ufes.png' description='Após terminar meu Ensino Médio, que foi atrelado ao Técnico em Mecânica, decidi que queria seguir a profissão de programador. Em função disso, entrei no curso de Engenharia da Computação, que curso na UFES de São Mateus.'/>
-                    </div>
-                    <div className="date-container date-r">
-                        <div className="date">SET/2021</div><div className="time-circle c-2"></div>
-                        <CardComponent name='Adapti' logo='logo-adapti.png' description='Entrei na Empresa Junior da Computação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como programar e ter a contato com o mundo do desenvolvimento web.'/>
-                    </div>  
+                    <CardComponent name='UFES' logo='logo-ufes.png' description='Após terminar meu Ensino Médio, que foi atrelado ao Técnico em Mecânica, decidi que queria seguir a profissão de programador. Em função disso, entrei no curso de Engenharia da Computação, que curso na UFES' time='ABR/2021'/>
+                    <CardComponent name='Adapti' logo='logo-adapti.png' description='Entrei na Empresa Junior da Computação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como programar e ter a contato com o mundo do desenvolvimento web.' time='SET/2021'/>
                     <div className="time-line-gradient"></div>
                 </div>
             <div className="time-line-container"></div>
