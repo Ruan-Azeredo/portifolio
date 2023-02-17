fcb360b0692676a278daa5bed420440d7a12b2a9
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-16 23:55:11 -0300

    :construction: build: add styles tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#202020',
+        'gray': '#3d3d3d',
+        'cinzaclaro': '#909090',
+        'azulescuro': '#152DFF',
+        'azulclaro': '#41ABCC',
+        'background': '#2C3333'
+      }
+    },
   },
   plugins: [],
 }
