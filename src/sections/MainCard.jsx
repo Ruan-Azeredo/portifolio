ebc5a0b99f2c065259f0b55abceb2df5857b779b
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-17 22:14:00 -0300

    :recycle: refactor: main glass card

--- src/sections/MainCard.jsx
+++ src/sections/MainCard.jsx
@@ -1,29 +1,33 @@
 function MainCard() {
     return (
-        <div className='ruan-card glass'>
-            <div className='myself-container'>
-            <div className='align-content'>
-                <img src="minhaft.png" className='minhaft'/>
-                <p className='meunome'>Ruan Azeredo</p>
-            </div>
-            </div>
-            <div className='rightside-container'>
+        <div className="main-card-section">
             <div className='navbar'>
                 <span className='navbar-item'><a href="#sobremim">SOBRE MIM</a></span>
                 <span className='navbar-item'><a href="#projetos">PROJETOS</a></span>
                 <span className='navbar-item'><a href="#trajetoria">TRAJETORIA</a></span>
                 <span className='navbar-item'><a href="#contato">CONTATO</a></span>
             </div>
-            <div className='front-dev-container'>
-                <div className='align-content'>
-                <div className='frontend'>FRONT-END</div>
-                <div className='developer'>DEVELOPER</div>
-                <a className='github-container' href="https://github.com/Ruan-Azeredo">
-                    <img src='github-icon.png' className='github-icon' />
-                    <p className='github-link'>github.com/Ruan-Azeredo</p>
-                </a>
+            <div className="rectangle"></div>
+            <div className='ruan-card glass'>
+                <div className="r-infos">
+                    <div className="name n-white">Ruan Azeredo</div>
+                    <div className="name n-gradient">Ruan Azeredo</div>
+                    <div className="name-subtitle">
+                    Alguém que adora música, café e JavaScript e <span>Front-end</span>.
+                    </div>
+                    <div className="name-subtitle">
+                    Vamos construir algo incrível juntos
+                    </div>
+                    <div className="btn-github">
+                        <a className='github-container' href="https://github.com/Ruan-Azeredo">
+                            <img src='github-icon.png' className='github-icon' />
+                            <p className='github-link'>github.com/Ruan-Azeredo</p>
+                        </a>                
+                    </div>
                 </div>
-            </div>
+                <div className="l-photo">
+                    <img src="mask-group.png" alt="" />
+                </div>
             </div>
         </div>
     )
