d93ce55ec4302316b534239c920457586dc2719b
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-20 16:12:27 -0300

    :recycle: refactor: prepara CardComponent p data

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,7 +1,9 @@
 export default function CardComponent(props) {
 
+    const infos = props.infos
+
     var fl, end, start
-    if (props.name == 'Adapti') {
+    if (infos.name == 'Adapti') {
         fl = 'flex-col-reverse'
         end = 'items-end'
         start = 'items-start'
@@ -16,20 +18,20 @@ export default function CardComponent(props) {
             <div className="bg-black h-48 p-4 flex flex-col rounded-lg gap-2 items-start drop-shadow-xl relative md:-left-5 hover:-rotate-2 transition ease-in-out delay-50">
                 <div className="flex w-full justify-between">
                     <div className="bg-gradient-to-br from-white to-gray w-14 h-14 grid items-center p-2 rounded-lg drop-shadow-xl">
-                        <img src={props.logo} alt=""/>
+                        <img src={infos.logo} alt=""/>
                     </div>''
-                    <div className="font-semibold text-lg text-white mx-5 my-2">{props.name}</div>
+                    <div className="font-semibold text-lg text-white mx-5 my-2">{infos.name}</div>
                 </div>
                 <div className="text-sm text-white pr-2 max-w-sm w-fit">
                     
-                    <div className="text-justify">{props.description}</div>
+                    <div className="text-justify">{infos.description}</div>
                 </div>
             </div>
             <div className="bg-white rounded-full h-3 w-3 mx-10 my-3 hidden md:block">
 
             </div>
             <div className={`h-48 ${end} hidden md:grid`}>
-                <div className="bg-white px-3 py-1 rounded-lg w-fit">{props.time}</div>
+                <div className="bg-white px-3 py-1 rounded-lg w-fit">{infos.time}</div>
             </div>
         </div>
     )
