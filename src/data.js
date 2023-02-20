f00506536d8535bd910a6c82810e5a2579c5a8e8
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-20 16:11:08 -0300

    :sparkles: feat: adiciona data em arq separado

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const projetos = [
+    {
+        id: 1,
+        name: 'MyBooks',
+        icons: ['next-icon.png', 'node-icon.png', 'firebase-icon.png'],
+        tags: ['Full-Stack', 'Sistema', 'API Rest'],
+        screen: 'telamybooks.png',
+        description: 'Este projeto consiste em uma aplicação onde o usuário pode cadastrar seus livros e resenhas do mesmo, para que assim tenha um lugar organizado e intuitivo para deixar suas reflexões sobre os livros lidos. O MyBooks foi desenvolvido com o Front-end e o Back-end separados, que se comunicam via API.',
+        link: 'https://github.com/Ruan-Azeredo/MyBooks',
+        adapti: true,
+        seeMore: '/mybooks'
+    },
+    {
+        id: 2,
+        name: 'e-diaristas',
+        icons: ['next-icon.png', 'stayled-icon.png', 'laravel-icon.png'],
+        tags: ['tag de teste', 'outro teste'],
+        screen: 'telaediaristas.png',
+        description: 'Este projeto foi desenvolvido no bootcamp da TreinaWeb, e consiste em um sistema com Back-end em Laravel onde pode-se cadastrar diaristas e gera uma API. A parte do Front-end foi desenvolvida em Next.js, onde o usuário digita um cep e o sistema mostra os diaristas presentes proximas ao cep.',
+        link: 'https://github.com/Ruan-Azeredo/Bootcamp-Next-e-diaristas'
+    },
+    {
+        id: 1,
+        name: 'Crud',
+        icons: ['react-icon.png', 'node-icon.png', 'tailwind-icon.png'],
+        tags: ['lorem inpsum'],
+        screen: 'telamybooks.png',
+        description: 'Um CRUD, que é uma aplicação simples que permite adicionar, ver, editar e excluir informações. Neste projeto o CRUD trabalha com usuários, coletando seus nomes e emails, alem de gera um ID para cada. O Front-end deste projeto foi desenvolvido em React.js, e o Back-end em Node.js.',
+        link: 'https://github.com/Ruan-Azeredo/crud-usuarios-React-'
+    }
+]
+
+export const sobreMim = 'Ola, meu chamo Ruan, tenho 20 anos e estou trilhando este caminho de Desenvolvedor Web. Gosto muito de desenvolver interfaces Front-end, porem estou sempre aberto a desenvolver o Back-end de algumas aplicações, afinal, gosto de construir aplicações completas que possa ver funcionando no final. Sou um grande fã de JavaScript e gosto de desenvolver alguns designs também. Gosto de estar sempre aberto para novas tecnologias.'
+
+export const trajetorias = [
+    {
+        name: 'UFES',
+        logo: 'logo-ufes.png',
+        description: 'Após terminar meu Ensino Médio, que foi atrelado ao Técnico em Mecânica, decidi que queria seguir a profissão de programador. Em função disso, entrei no curso de Engenharia da Computação, que curso na UFES.',
+        time: 'ABR/2021'
+    },
+    {
+        name: 'Adapti',
+        logo: 'logo-adapti.png',
+        description: 'Entrei na Empresa Junior da Computação da Faculdade, empresa a qual não tem fins lucrativos para os membros, e que faço parte até hoje, nela aprendi como programar e ter a contato com o mundo do desenvolvimento web.',
+        time: 'SET/2021',
+    }
+]
