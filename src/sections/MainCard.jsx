0efe14e7b7830b6959dbeaf381291c1c1e3afb11
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-24 09:23:38 -0300

    :recycle: refactor: muda todo o design main card

--- src/sections/MainCard.jsx
+++ src/sections/MainCard.jsx
@@ -1,32 +1,43 @@
+import RuanAzeredo from "../components/RuanAzeredo";
+
 function MainCard() {
     return (
         <div className="main-card-section">
             <div className='navbar'>
-                <span className='navbar-item'><a href="#sobremim">SOBRE MIM</a></span>
-                <span className='navbar-item'><a href="#projetos">PROJETOS</a></span>
-                <span className='navbar-item'><a href="#trajetoria">TRAJETORIA</a></span>
-                <span className='navbar-item'><a href="#contato">CONTATO</a></span>
+                <div>
+                    <a href="/"  className="w-8 h-8"><img src="perfil.png" alt="" /></a>
+                </div>
+                <div>
+                    <span className='navbar-item'><a href="#sobremim">SOBRE MIM</a></span>
+                    <span className='navbar-item'><a href="#projetos">PROJETOS</a></span>
+                    <span className='navbar-item'><a href="#trajetoria">TRAJETORIA</a></span>
+                    <span className='navbar-item'><a href="#contato">CONTATO</a></span>
+                </div>
             </div>
             <div className="rectangle"></div>
             <div className='ruan-card glass'>
                 <div className="r-infos">
-                    <div className="name n-white">Ruan Azeredo</div>
-                    <div className="name n-gradient">Ruan Azeredo</div>
-                    <div className="name-subtitle">
+                    <RuanAzeredo/>
+                    {/* <div className="name n-white">Ruan Azeredo</div>
+                    <div className="name n-gradient">Ruan Azeredo</div> */}
+                    <div className="name-subtitle desk-sub">
                     Alguém que adora música, café e JavaScript e <span>Front-end</span>.
                     </div>
-                    <div className="name-subtitle">
-                    Vamos construir algo incrível juntos
+                    <div className="name-subtitle mobi-sub">
+                        Front-end Developer
+                    </div>
+                    <div className="name-subtitle desk-sub">
+                    Vamos construir algo incrível juntos!
                     </div>
                     <div className="btn-github">
-                        <a className='github-container' href="https://github.com/Ruan-Azeredo">
+                        <a className='github-container border-grayy border-2' href="https://github.com/Ruan-Azeredo">
                             <img src='github-icon.png' className='github-icon' />
                             <p className='github-link'>github.com/Ruan-Azeredo</p>
                         </a>                
                     </div>
                 </div>
                 <div className="l-photo">
-                    <img src="mask-group.png" alt="" />
+                    <img src="picwish.png" alt="" />
                 </div>
             </div>
         </div>
