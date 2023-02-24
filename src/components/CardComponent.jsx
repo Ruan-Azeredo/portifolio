d3dd469e3806902bacd602e2500c527e45d91d47
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-24 00:38:32 -0300

    :iphone: feat: responsiva e muda card trajetoria

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -14,10 +14,10 @@ export default function CardComponent(props) {
     }
 
     return (
-        <div className={`flex ${fl} relative -top-[209px] z-30 px-5 md:px-20`}>
-            <div className="bg-black h-48 p-4 flex flex-col rounded-lg gap-2 items-start drop-shadow-xl relative md:-left-5 hover:-rotate-2 transition ease-in-out delay-50">
+        <div className={`flex ${fl} relative -top-[100px] md:-top-[209px] z-30 px-5 md:px-20`}>
+            <div className="bg-black border-grayy border-2 h-fit md:h-48 p-4 flex flex-col rounded-lg gap-2 items-start drop-shadow-xl relative md:-left-5 hover:-rotate-2 transition ease-in-out delay-50">
                 <div className="flex w-full justify-between">
-                    <div className="bg-gradient-to-br from-white to-gray w-14 h-14 grid items-center p-2 rounded-lg drop-shadow-xl">
+                    <div className="bg-gradient-to-br from-white to-grayy w-14 h-14 grid items-center p-2 rounded-lg drop-shadow-xl">
                         <img src={infos.logo} alt=""/>
                     </div>''
                     <div className="font-semibold text-lg text-white mx-5 my-2">{infos.name}</div>
@@ -30,7 +30,7 @@ export default function CardComponent(props) {
             <div className="bg-white rounded-full h-3 w-3 mx-10 my-3 hidden md:block">
 
             </div>
-            <div className={`h-48 ${end} hidden md:grid`}>
+            <div className={`h-fit md:h-48 ${end} hidden md:grid`}>
                 <div className="bg-white px-3 py-1 rounded-lg w-fit">{infos.time}</div>
             </div>
         </div>
