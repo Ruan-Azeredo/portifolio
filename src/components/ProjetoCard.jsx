fb11f91ae82448e8ea1a7ab01778f8121ba4d661
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-24 09:22:21 -0300

    :recycle: refactor: refaz card de Projeto design

--- src/components/ProjetoCard.jsx
+++ src/components/ProjetoCard.jsx
@@ -1,23 +1,92 @@
 export default function ProjetoCard(props) {
+    const infos = props.infos
+
+    var flexDirection, hidden
+    infos.id % 2 == 0 ? flexDirection = 'md:flex-row-reverse' : flexDirection = 'md:flex-row'
+    infos.adapti == true ? hidden = null : hidden = 'hidden'
+
     return (
-        <div className="gradient-bg">
-            <div className="inside-bg">
-                <div className="card-black">
-                    <div className="nav-card">
-                        <div className="card-name">{props.name}</div>
-                        <div className="icons-card">
-                            <img src={props.icon1} alt="" />
-                            <img src={props.icon2} alt="" />
-                            <img src={props.icon3} alt="" />
+        <div className={`ProjetoCard flex flex-col ${flexDirection} group px-5 md:px-20 gap-10 relative justify-between`}>
+            <div className="md:w-1/2 text-white flex flex-col gap-4">
+                <div className="text-xl my-4 name-prj">{infos.name}</div>
+                <div className="text-[14px] md:text-[16px] text-gray-300">{infos.description}</div>
+                <div className="flex gap-4 py-2">
+                    {infos.icons?.map(item => (
+                        <div key={item}>
+                            <img className="h-8" src={item} alt="" />
                         </div>
-                    </div>
-                    <img className="img-card" src={props.screen} alt="" />
-                    <div className="infos-card">
-                        <p className="txt-card">{props.description}</p>
-                        <a href={props.link} className="git-button"><img src="github-icon.png" alt=""/></a>
-                    </div>
+                    ))}
+                </div>
+                <div className="flex gap-2 py-2">
+                    {infos.tags?.map(item => (
+                        <div key={item} className="bg-black px-3 py-1 rounded-md">{item}</div>
+                    ))}
+                </div>
+                <div className="flex gap-4">
+                    <a href={infos.link} className="h-8 flex py-1 px-8 w-fit border-2 rounded-md">
+                        <img src="github-icon.png" alt="" />
+                    </a>
+                    {infos.seeMore ? (
+                        <a href={infos.seeMore} className="py-1 px-3 w-fit border-2 rounded-md text-sm">VER MAIS +</a>
+                    ) : null}
                 </div>
             </div>
+            <div className="relative justify-end flex saturate-0 group-hover:saturate-100 group-hover:-translate-y-2 transition ease-in-out delay-150">
+                <img src={infos.screen} alt="" />
+                <div className={`${hidden} bg-gradient-to-r from-azulescuro to-azulclaro text-white font-medium rounded-md py-1 px-3 w-fit absolute top-40 md:top-72 md:right-2`}>Adapti</div>
+            </div>
+            <style jsx>{`
+                .ProjetoCard {
+                    font-family: 'Inter';
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 16px;
+                    line-height: 22px;
+                    text-align: justify;
+                }
+
+                .name-prj{
+                    width: fit-content;
+                    display: flex;
+                    flex-direction: column
+                }
+
+                .name-prj::after{
+                    content: '';
+                    width: 0px;
+                    height: 3px;
+                    position: relative;
+                    border-radius: 1px;
+                    
+                    background: linear-gradient(to right, var(--azulescuro), var(--azulclaro));
+                    transition: width 0.3s;
+                }
+
+                .ProjetoCard:hover .name-prj::after{
+                    width: 120%;
+                }
+            `}</style>
         </div>
+
+
+        // <div className="gradient-bg">
+        //     <div className="inside-bg">
+        //         <div className="card-black">
+        //             <div className="nav-card">
+        //                 <div className="card-name">{infos.name}</div>
+        //                 <div className="icons-card">
+        //                     <img src={infos.icon1} alt="" />
+        //                     <img src={infos.icon2} alt="" />
+        //                     <img src={infos.icon3} alt="" />
+        //                 </div>
+        //             </div>
+        //             <img className="img-card" src={infos.screen} alt="" />
+        //             <div className="infos-card">
+        //                 <p className="txt-card">{infos.description}</p>
+        //                 <a href={infos.link} className="git-button"><img src="github-icon.png" alt=""/></a>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
