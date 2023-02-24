c31da716f13e0e6af85ad4dcf2848f6ffb02bc5c
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-02-24 09:26:53 -0300

    :goal_net: fix: coloca titulo em maiusculo

--- src/sections/Trajetoria.jsx
+++ src/sections/Trajetoria.jsx
@@ -4,15 +4,15 @@ import { trajetorias } from "../data";
 
 export default function Trajetoria() {
     return (
-        <div className="trajetoria-container"><a name='trajetoria'></a>
-            <Title title='Trajetoria' grayscale={true}/>
-                <div className="time-line relative top-16 gap-8 md:gap-0">
-                {trajetorias.map(item => (
-                        <CardComponent infos={item}/>
-                    ))}
-                    <div className="time-line-gradient"></div>
-                </div>
-            <div className="time-line-container"></div>
+        <div className="trajetoria-container pt-24"><a name='trajetoria'></a>
+            <Title title='TRAJETORIA' grayyscale={true}/>
+            <div className="time-line relative top-16 gap-8 md:gap-0">
+            {trajetorias.map(item => (
+                    <CardComponent infos={item}/>
+                ))}
+                <div className="time-line-gradient"></div>
+            </div>
+            {/* <div className="time-line-container"></div> */}
         </div>
     )
 }
