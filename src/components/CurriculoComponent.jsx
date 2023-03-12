6a6039732a9400f62e2c1c9d463bfea2bf1574f5
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-12 11:01:22 -0300

    :construction: build: translate curriculo button

--- src/components/CurriculoComponent.jsx
+++ src/components/CurriculoComponent.jsx
@@ -1,4 +1,12 @@
-export default function CurriculoComponent() {
+export default function CurriculoComponent(props) {
+    var lang = props.lang
+
+    var a
+    if (lang == 'PT' || lang == null) {
+        a = 'VEJA MEU URRICULO'
+    } else {
+        a = 'SEE MY RESUME'
+    }
     return (
         <div className=" backgr-black black-card-curriculo border-grayy border-2">
             <div className="border-gradient curriculo-border">
@@ -6,7 +14,7 @@ export default function CurriculoComponent() {
                     <img src="gen/A4-1.png" alt="" className="curriculo-img"/>
                 </div>
             </div>
-            <a className="link-curriculo" href="Curriculo-Ruan.pdf" download>VEJA MEU CURRICULO</a>
+            <a className="link-curriculo" href="Curriculo-Ruan.pdf" download>{a}</a>
         </div>
     )
 }
