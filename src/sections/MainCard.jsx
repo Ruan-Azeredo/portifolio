bdd4e3715008bf021813ca815947c695eba9ab7f
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-12 11:09:25 -0300

    :sparkles: feat: add button translate

--- src/sections/MainCard.jsx
+++ src/sections/MainCard.jsx
@@ -1,22 +1,51 @@
+import { Button } from "@nextui-org/react";
+import { useContext } from "react";
 import ModalButton from "../components/ModalButton";
 import RuanAzeredo from "../components/RuanAzeredo";
+import LanguageContext from "../context/LanguageContext";
+import { Language } from "../icons/Language";
 
 function MainCard() {
+    const { lang, alterarLingua } = useContext(LanguageContext)
+
+    var p1, p2, l
+    if (lang == 'PT' || lang == null) {
+        p1 = 'Alguém que adora música, café, JavaScript e '
+        p2 = 'Vamos construir algo incrível juntos!'
+        l = 'P'
+    } else {
+        p1 = 'Someone who loves music, coffee, JavaScript and '
+        p2 = "Let's build something amazing together!"
+        l = 'E'
+    }
+
     return (
         <div className="main-card-section">
+                    <div className="md:hidden justify-end grid">
+                        <Button
+                            onClick={() => alterarLingua()}
+                            auto color="#152DFF" rounded bordered
+                            css={{background: "transparent", border: "none", h: 32}}
+                        ><Language className='scale-125' /></Button>
+                    </div>
             <div className='navbar'>
-                <div>
+                <div className="gap-8 div">
                     {/* <a href="/" className="w-8 h-8"> */}
                     <ModalButton img="gen/perfilSquare.png">
-                        <img src="gen/perfilSquare.png" alt="" className="w-8 h-8 rounded-full"/>
+                        <img src="gen/perfilSquare.png" alt="" className="w-8 h-8 rounded-full m-0"/>
                     </ModalButton>
                     {/* </a> */}
+                    <Button
+                        onClick={() => alterarLingua()}
+                        auto color="#152DFF" rounded bordered
+                        css={{background: "transparent", border: "none", h: 32}}
+                    ><Language /></Button>
                 </div>
-                <div>
-                    <span className='navbar-item'><a href="#sobremim">SOBRE MIM</a></span>
-                    <span className='navbar-item'><a href="#projetos">PROJETOS</a></span>
-                    <span className='navbar-item'><a href="#trajetoria">TRAJETORIA</a></span>
-                    <span className='navbar-item'><a href="#contato">CONTATO</a></span>
+                <div className="justify-between div">
+                    <span className='navbar-item'><a href="#sobremim">{l == 'P' ? 'SOBRE MIM' : 'ABOUT ME'}</a></span>
+                    <span className='navbar-item'><a href="#projetos">{l == 'P' ? 'PROJETOS' : 'PROJECTS'}</a></span>
+                    <span className='navbar-item'><a href="#trajetoria">{l == 'P' ? 'TRAJETORIA' : 'TRAJECTORY'}</a></span>
+                    <span className='navbar-item'><a href="#contato">{l == 'P' ? 'CONTATO' : 'CONTACT'}</a></span>
                 </div>
             </div>
             <div className="rectangle"></div>
@@ -26,13 +55,13 @@ function MainCard() {
                     {/* <div className="name n-white">Ruan Azeredo</div>
                     <div className="name n-gradient">Ruan Azeredo</div> */}
                     <div className="name-subtitle desk-sub">
-                    Alguém que adora música, café, JavaScript e <span>Front-end</span>.
+                    {p1}<span>Front-end</span>.
                     </div>
                     <div className="name-subtitle mobi-sub">
                         Web Developer
                     </div>
                     <div className="name-subtitle desk-sub">
-                    Vamos construir algo incrível juntos!
+                    {p2}
                     </div>
                     <div className="btn-github">
                         <a className='github-container border-grayy border-2' href="https://github.com/Ruan-Azeredo">
